8e3d157a309caa7ac429be8202acd009f7e12975
com <com@DESKTOP-UV1O9DM>
2022-10-29 17:11:01 +0530

    First_Commit1

---
+++ src/Basic/test1.java
@@ -0,0 +1,27 @@
+package Basic;
+
+public class test1
+{
+	public static void main(String[] args) {
+		
+		System.out.println("Hii");
+		
+		  M1();
+		
+		
+		
+	}
+	
+	public static void M1() 
+	{
+		System.out.println("Good Morning");
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
